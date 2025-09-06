957947abed10d789d565879c23067fe0da45f872
Michal Latynski <michal@latynski.pl>
2025-09-06 03:01:22 +0200

    Migration to ToDo table in db

--- ToDoAPI.Dal/Data/TodoDbContext.cs
+++ ToDoAPI.Dal/Data/TodoDbContext.cs
@@ -3,11 +3,8 @@ using ToDoAPI.Dal.Entities;
 
 namespace ToDoAPI.Dal.Data
 {
-    public class TodoDbContext : DbContext
+    public class TodoDbContext(DbContextOptions<TodoDbContext> options) : DbContext(options)
     {
-        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
-        {
-        }
 
         // Register Task to make possible to create database structure by creating migration
         public DbSet<TaskEntity> Tasks { get; set; }
---
+++ ToDoAPI.Dal/Migrations/20250906005910_ToDoInit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoAPI.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class ToDoInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsCompleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PercentageComplete = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
