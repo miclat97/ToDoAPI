2951ce91485314452706d35a1f4bcb518f8c4976
Michal Latynski <michal@latynski.pl>
2025-09-06 13:22:45 +0200

    Repository interface implements base repository interface - fix

--- ToDoAPI.Dal/Repository/IToDoRepository.cs
+++ ToDoAPI.Dal/Repository/IToDoRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoAPI.Dal.Entities;
+using ToDoAPI.Dal.Repositories;
 
 namespace ToDoAPI.Dal.Repository
 {
-    public interface IToDoRepository
+    public interface IToDoRepository : IBaseRepository<TaskEntity>
     {
     }
 }
--- ToDoAPI.Dal/Repository/ToDoRepository.cs
+++ ToDoAPI.Dal/Repository/ToDoRepository.cs
@@ -9,7 +9,10 @@ using ToDoAPI.Dal.Repositories;
 
 namespace ToDoAPI.Dal.Repository
 {
-    public class ToDoRepository : BaseRepository<TaskEntity>, IToDoRepository
+    public class ToDoRepository : BaseRepository<TaskEntity>, IToDoRepository // Because it's inherited from base repository, there's no need to implement those
+                                                                              // "basic"/"standard" methods independently in each repository again. And it's still
+                                                                              // implementing it's own interface (which is implementing base repository interface as well)
+                                                                              // to have implemented modern design patterns implemented in whole project
     {
         public ToDoRepository(TodoDbContext context) : base(context)
         {
